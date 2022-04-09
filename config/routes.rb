f3b767b4b322429f81fff46bbfd7cca14ea77b00
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-04-09 11:06:47 +0500

    products added + image for products added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users, controllers: { registrations: 'users/registrations',omniauth_callbacks: 'users/omniauth_callbacks'  }
 
-  root 'shopingstore#index'
+  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
