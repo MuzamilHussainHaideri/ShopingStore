74bb45cca88dfc96bd3f739bbdbc1132476da762
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-04-09 14:46:20 +0500

    Styling is added to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user, optional: true
   has_one_attached :image
 
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
+  validates :name, length: { maximum: 140, too_long: "%{count} characters is the maximum aloud. "}
+  validates :price, length: { maximum: 7 }
+
 end
