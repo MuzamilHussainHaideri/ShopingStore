3f8fe711494d79bca609cf8fed969e8fba9beff7
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-04-22 15:55:40 +0500

    pay now button added and complete functionality of this button with stripe added

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
   devise_for :users, controllers: { registrations: 'users/registrations',omniauth_callbacks: 'users/omniauth_callbacks'  }
-
   root 'products#index'
+
+  post "checkout/create", to: "checkout#create"
+  get "success", to: "checkout#success"
+  get "cancel", to: "checkout#cancel"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
